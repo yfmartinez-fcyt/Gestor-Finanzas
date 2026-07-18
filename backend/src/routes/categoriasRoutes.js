82123ae829f328776949c9596e57e1b9a68ab73c
maroduarte <145401775+maroduarte@users.noreply.github.com>
2026-07-18 20:14:08 -0300

    Crea la ruta de Categorías

---
+++ backend/src/routes/categoriasRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const authMiddleware = require('../middleware/authMiddleware');
+
+const {
+    obtenerCategorias,
+    obtenerCategoriaPorId,
+    crearCategoria,
+    editarCategoria,
+    eliminarCategoria
+} = require('../controllers/categoriasController');
+
+
+router.use(authMiddleware);
+
+
+
+router.route('/')
+    .get(obtenerCategorias)
+    .post(crearCategoria);
+
+
+router.route('/:id')
+    .get(obtenerCategoriaPorId)
+    .put(editarCategoria)
+    .delete(eliminarCategoria);
+
+
+module.exports = router;
